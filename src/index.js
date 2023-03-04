451c906ef112bc38626ccfbc1bda505d8cc27b80
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-04 15:42:45 -0600

    Add buildtable function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
+  function buildTable() {
+    const table = document.getElementById("myTable");
+    table.innerHTML = "";
+    for (let i = 0; i < myTasks.length; i++) {
+      const row = `<tr>
+                        <td>${myTasks[i].task}</td>
+                        <td>${myTasks[i].description}</td>
+                        <td>${myTasks[i].due}</td>
+                        <td>${myTasks[i].priority}</td>
+                        <td><button onclick="removeTask(${i})">Remove</button></td> 
+                  </tr>`;
+      table.innerHTML += row;
+    }
+  }
+
+  function removeTask(index) {
+    myTasks.splice(index, 1);
+    buildTable();
+  }
+
   function addTask() {
     const task = document.querySelector("#task").value;
     const description = document.querySelector("#description").value;
@@ -19,8 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const priority = document.querySelector("#priority").value;
     const newTask = new taskItem(task, description, due, priority);
     myTasks.push(newTask);
+    buildTable();
     document.forms[0].reset();
+    console.log(myTasks);
   }
-  addTask();
-  console.log(myTasks);
+
+  document.getElementById("btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    addTask();
+  });
 });
