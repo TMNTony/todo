838acb1fa560245cac04ea913cbc91bef677a212
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-04 14:15:20 -0600

    Add factory/addTask function to load html inputs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import "./style.css";
 
+const myTasks = [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello World");
+  function taskItem(task, description, due, priority) {
+    return {
+      task,
+      description,
+      due,
+      priority,
+    };
+  }
+
+  function addTask() {
+    const task = document.querySelector("#task").value;
+    const description = document.querySelector("#description").value;
+    const due = document.querySelector("#dueBy").value;
+    const priority = document.querySelector("#priority").value;
+    const newTask = new taskItem(task, description, due, priority);
+    myTasks.push(newTask);
+    document.forms[0].reset();
+  }
+  addTask();
+  console.log(myTasks);
 });
